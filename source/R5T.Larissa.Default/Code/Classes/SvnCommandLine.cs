06763cc23279d270b847535f738b8cb6be3f1027
MinexAutomation <minex.automation@gmail.com>
2019-12-21 13:38:30 -0800

    Added SVN command line and SVN operator types.

---
+++ source/R5T.Larissa.Default/Code/Classes/SvnCommandLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.Heraklion;
+
+using R5T.Larissa.Commands;
+using R5T.Volos;
+
+
+namespace R5T.Larissa.Default
+{
+    public static class SvnCommandLine
+    {
+        public static ICommandBuilderContext<SvnContext> Start()
+        {
+            var commandBuilderContext = CommandLine.Start<SvnContext>();
+            return commandBuilderContext;
+        }
+    }
+}
---
+++ source/R5T.Larissa.Default/Code/Services/Implementations/SvnOperator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.Caledonia;
+using R5T.Heraklion;
+using R5T.Heraklion.Default;
+
+using R5T.Larissa.Configuration;
+using R5T.Larissa.Commands;
+
+
+namespace R5T.Larissa.Default
+{
+    public class SvnOperator : ISvnOperator
+    {
+        private ICommandLineInvocationOperator CommandLineInvocationOperator { get; }
+        private ISvnExecutableFilePathProvider SvnExecutableFilePathProvider { get; }
+
+
+        public SvnOperator(ICommandLineInvocationOperator commandLineInvocationOperator, ISvnExecutableFilePathProvider svnExecutableFilePathProvider)
+        {
+            this.CommandLineInvocationOperator = commandLineInvocationOperator;
+            this.SvnExecutableFilePathProvider = svnExecutableFilePathProvider;
+        }
+
+        private void Execute(ICommandBuilderContext command)
+        {
+            var gitExecutableFilePath = this.SvnExecutableFilePathProvider.GetSvnExecutableFilePath();
+
+            this.CommandLineInvocationOperator.Execute(gitExecutableFilePath, command);
+        }
+    }
+}
