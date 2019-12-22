d435acf136c68b322c227c8a8bb1d5a80356f035
MinexAutomation <minex.automation@gmail.com>
2019-12-21 22:38:23 -0800

    Added SvnversionOperator implementation (nothing to implement yet).

---
+++ source/R5T.Larissa.Default/Code/Services/Implementations/SvnversionOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.Larissa.Default
+{
+    public class SvnversionOperator : ISvnversionOperator
+    {
+        private ILogger Logger { get; }
+
+
+        public SvnversionOperator(ILogger<SvnversionOperator> logger)
+        {
+            this.Logger = logger;
+        }
+    }
+}
